35d4355201f0f3f8171750528f9c9e0ec3842ae6
ayasaber1 <85575658+ayasaber1@users.noreply.github.com>
2022-04-13 00:43:52 +0200

    Add files via upload

---
+++ template_sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+template<class t1,class t2>
+void sum(t1 a,t2 b) 
+{
+    cout<<"\nSum="<<a+b<<endl;
+}
+ 
+int main()
+{
+    int a,b;
+    float x,y;
+    cout<<"\nEnter two integer data: ";
+    cin>>a>>b;
+    cout<<"\nEnter two float data: ";
+    cin>>x>>y;
+    sum(a,b); 
+    sum(x,y);      
+}
